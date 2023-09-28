e0ae11271c3df0406e378a52206f9da4df954399
juanesosorio <Juanchis_1989@hotmail.com>
2023-09-27 22:54:49 -0500

    Se avanza en la parte WEB

---
+++ EduEventPlatform.API/Migrations/20230928031150_InitialDb.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduEventPlatform.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AcademicEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    EventTheme = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AcademicEvents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventsSchedule",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SessionName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Speaker = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    SessionTopic = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventsSchedule", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AcademicEvents_EventName",
+                table: "AcademicEvents",
+                column: "EventName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventsSchedule_SessionName",
+                table: "EventsSchedule",
+                column: "SessionName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AcademicEvents");
+
+            migrationBuilder.DropTable(
+                name: "EventsSchedule");
+        }
+    }
+}
--- EduEventPlatform.WEB/Program.cs
+++ EduEventPlatform.WEB/Program.cs
@@ -1,11 +1,15 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using EduEventPlatform.WEB;
+using EduEventPlatform.WEB.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+// HttpClient para consumir el API desde la Web
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000/") });
+//Inyectamos el patrón Repository al proyecto WEB
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddSweetAlert2();
 await builder.Build().RunAsync();
